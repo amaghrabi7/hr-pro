d1d24032322002c9912ebfb95667eea50e3ab1a7
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-02 18:46:44 +0300

    Created the Manager class.

--- hr_pro.py
+++ hr_pro.py
@@ -1,9 +1,9 @@
 class Employee:
-   def __init__(self, name, age, salary, employment_years):
-    self.name = name
-    self.age = age
-    self. salary = salary
-    self.employment_years = employment_years
+    def __init__(self, name, age, salary, employment_years):
+        self.name = name
+        self.age = age
+        self. salary = salary
+        self.employment_years = employment_years
 
     def __str__(self):
         return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Working Years: {self.employment_years}"
@@ -11,11 +11,24 @@ class Employee:
     def get_annual_salry(self):
         return self.salary * 12
 
+employee = Employee("ahmed", 23, 1000, 3)
+print(employee)
+
 class Manager(Employee):
-    #Manager class here
-        
-def main():
+    def __init__(self, name, age, salary, employment_years, bonus_percentage):
+        super().__init__(name, age, salary, employment_years)
+        self.bonus_percentage = bonus_percentage
+    
+    def __str__(self):
+        return f"{super().__str__()}, Bonus: {self.get_bonus()}"
+    
+    def get_bonus(self):
+        return self.bonus_percentage * self.salary
+
+manager = Manager("aziz", 45, 5000, 20, 0.2) 
+print(manager)       
+# def main():
 	# main code here
 
-if __name__ == '__main__':
-	main()
+# if __name__ == '__main__':
+# 	main()
