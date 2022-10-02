893d68b15f548015e371af4490f883143517a604
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-02 18:36:38 +0300

    Created Employee class.

--- hr_pro.py
+++ hr_pro.py
@@ -1,5 +1,15 @@
 class Employee:
-   #Employee class here
+   def __init__(self, name, age, salary, employment_years):
+    self.name = name
+    self.age = age
+    self. salary = salary
+    self.employment_years = employment_years
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Salary: {self.salary}, Working Years: {self.employment_years}"
+    
+    def get_annual_salry(self):
+        return self.salary * 12
 
 class Manager(Employee):
     #Manager class here
