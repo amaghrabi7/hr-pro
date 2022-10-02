3057d0c72f0b384925fa0aeba295cdf00a3983df
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-02 20:01:57 +0300

    Cleaned the main function by removing the objects from it.

--- hr_pro.py
+++ hr_pro.py
@@ -23,19 +23,21 @@ class Manager(Employee):
     def get_bonus(self):
         return self.bonus_percentage * self.salary
 
+employee_1 = Employee("Ahmed", 23, 1000, 3)
+employee_2 = Employee("Sarah", 25, 1300, 5)
+employee_3 = Employee("Mike", 28, 700, 2)
+manager_1 = Manager("Aziz", 45, 5000, 20, 0.2) 
+manager_2 = Manager("Khalid", 36, 4000, 15, 0.15)
+manager_3 = Manager("Zain", 40, 5500, 16, 0.25)
 
 def main():
-    employee_1 = Employee("Ahmed", 23, 1000, 3)
-    employee_2 = Employee("Sarah", 25, 1300, 5)
-    employee_3 = Employee("Mike", 28, 700, 2)
-    manager_1 = Manager("Aziz", 45, 5000, 20, 0.2) 
-    manager_2 = Manager("Khalid", 36, 4000, 15, 0.15)
-    manager_3 = Manager("Zain", 40, 5500, 16, 0.25)
     employees = [employee_1.__str__(), employee_2.__str__(), employee_3.__str__()]
     managers = [manager_1.__str__(), manager_2.__str__(), manager_3.__str__()]
 
     print("Welcome to HR Pro\nQuestions:\n1. Show employees\n2. Show managers\n3. Add an employee\n4. Add a manager\n5. Exit")
+    
     option = int(input("\nWhat would you like to do? "))
+    
     while not option == 5:
         if option == 1:
             for employee in employees:
